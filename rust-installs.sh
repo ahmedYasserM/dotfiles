b4df64fe9c4ce1d0c8ddba553b58314b1d37f9fa
Ahmed Yasser <ahmedyassermohammad@gmail.com>
2023-07-07 08:04:59 +0300

    adding rust crates

---
+++ rust-installs.sh
@@ -0,0 +1,7 @@
+cargo install zoxide
+cargo install lsd
+sudo apt-get install libxcb1-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev
+cargo install jless
+cargo install cargo-watch
+cargo install felix
+cargo install nu
