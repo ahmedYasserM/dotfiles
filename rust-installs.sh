63c88ad7e364b555f00a4e9521e46bbddf4e0a28
Ahmed Yasser <ahmedyassermohammad@gmail.com>
2023-07-09 23:10:54 +0000

    update cargo crates

--- rust-installs.sh
+++ rust-installs.sh
@@ -3,5 +3,7 @@ cargo install lsd
 sudo apt-get install libxcb1-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev
 cargo install jless
 cargo install cargo-watch
-cargo install felix
 cargo install nu
+cargo install dysk
+cargo install git-cliff
+cargo install git-delta
