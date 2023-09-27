5e9db304304b0e122dd7f5868893d4dec5096753
Ahmed Yasser <ahmedYasser.2592@gmail.com>
2023-09-27 13:15:12 +0300

    updating fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -8,5 +8,5 @@
 
 
 function fish_prompt
-	echo -n (set_color -o "#A7C080")(basename $PWD) "🡢 "
+    echo -n (set_color -o "#7AA2F7") (set_color -o "#9D98F7") (basename $PWD) (set_color -o "#9ECE6A") " "
 end
