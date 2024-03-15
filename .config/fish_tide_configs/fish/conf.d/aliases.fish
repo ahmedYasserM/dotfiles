75f715ad5d25c9d7720b7e2dd8f1aadf1180a499
Ahmed Yasser <ahmedYasser9@proton.me>
2024-03-15 15:00:46 +0200

    adding tide configs for fish shell

---
+++ .config/fish_tide_configs/fish/conf.d/aliases.fish
@@ -0,0 +1,98 @@
+#!/usr/bin/env fish
+
+# vim
+# alias vim "lvim"
+
+# dolphin
+alias do='setsid dolphin $1 &> /dev/null'
+
+# virtualbox
+alias vbox='vboxmanage'
+
+# ls
+alias  l='eza -lh  --icons=auto' # long list
+alias ls='eza   --icons=auto' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto' # long list dirs
+
+
+# tree symbols
+alias tsym "echo ├ ─ │ └"
+
+# bash
+alias o "bash open ."
+
+
+alias grep "grep --color=auto"
+
+alias cat "bat"
+
+alias ip "ip -c"
+
+alias c "clear -x"
+
+
+# matlab
+# alias matlab="matlab & disown"
+
+alias git-graph "git-graph --style round"
+
+alias cat "bat -p"
+
+alias ip "ip -c"
+
+alias c "clear -x"
+
+# cd
+alias .. "z .."
+alias ... "..;.."
+alias .... "...;.."
+
+
+alias grep "grep --color=auto"
+
+
+# obsidian
+alias to 'cp -r ~/repos/personal/dotfiles/templates/obsidian-template/{.obsidian,.obsidian.vimrc} .'
+
+# tree symbols
+alias tsym "echo ├ ─ │ └"
+
+# bash
+alias o "bash open ."
+
+# history
+alias history 'history'
+# alias history 'history | nl'
+
+
+# lvim
+alias vim 'nvim'
+
+
+# current time
+alias ctime 'date +%T'
+
+
+#sc-im
+alias scim="sc-im"
+
+# tmux
+alias  tl 'tmux ls'
+alias  tns 'tmux new-session -t'
+alias  ta 'tmux attach -t'
+alias  td 'tmux detach'
+
+# xbps
+# alias s 'doas xbps-install -S'
+# alias i 's;doas xbps-install'
+# alias q 'doas xbps-query -Rs'
+# alias u 's;doas xbps-install -u xbps; doas xbps-install -u'
+# alias r 'doas xbps-remove -R'
+
+# apt
+# alias r="sudo apt remove"       ## remove
+# alias q="apt search"       ## query
+# alias i="sudo apt install"      ## install
+# alias s="sudo apt update"       ## sync
+# alias u="sync;sudo apt upgrade" ## update
---
+++ .config/fish_tide_configs/fish/conf.d/vars.fish
@@ -0,0 +1,31 @@
+#!/usr/bin/env fish
+
+
+set -gx PATH "/home/ahmed/downloads/apps/bin" "/home/ahmed/.nvm/versions/node/v21.2.0/bin" "/home/ahmed/dev/go/bin" "/bin" "/home/ahmed/.cargo/bin" "/home/ahmed/dev/scripts/" "/home/ahmed/.local/bin"  "/sbin" "/snap/bin" "/usr/bin"  "/usr/local/bin"  "/usr/local/go/bin" "/usr/local/sbin" "/usr/sbin" 
+
+set -gx GOPATH "/home/ahmed/.go"
+
+set TERM kitty
+set -gx HISTSIZE 10000
+set -gx SAVEHIST 10000
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -g fish_greeting
+set -gx GOPATH "/home/ahmed/dev/go"
+
+# Manpages
+set -x MANPAGER "nvim +Man!"
+
+# nnn
+set -gx NNN_PLUG 'f:finder;o:fzopen;p:mocq;d:diffs;t:nmount;v:imgview;k:autojump'
+
+# tide
+set --universal tide_left_prompt_items os pwd git newline character
+set --universal tide_right_prompt_items status
+set --universal tide_pwd_color_dirs FFFFFF
+set --universal tide_pwd_color_anchors FFFFFF
+set --universal git_icon ' '
+set --universal _tide_pwd_len 1
+
+
+# tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time=No --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
