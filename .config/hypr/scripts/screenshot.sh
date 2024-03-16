160d578d9989a0c88d739ce521f0a718b46d2016
Ahmed Yasser <ahmedYasser9@proton.me>
2024-03-17 01:36:27 +0200

    updating hyprland configs

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -7,7 +7,7 @@ fi
 ScrDir=`dirname "$(realpath "$0")"`
 source $ScrDir/globalcontrol.sh
 swpy_dir="${XDG_CONFIG_HOME:-$HOME/.config}/swappy"
-save_dir="${2:-$XDG_PICTURES_DIR/Screenshots}"
+save_dir="${2:-$XDG_PICTURES_DIR/screenshots}"
 save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
 temp_screenshot="/tmp/screenshot.png"
 
