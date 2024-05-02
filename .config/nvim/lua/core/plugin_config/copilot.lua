3b9a326c66aa9273ed3a7c09ad9f6616a60434b8
Ahmed Yasser <ahmedYasser9@proton.me>
2024-05-02 06:48:18 +0300

    map alt+; to accept copilot suggestion

--- .config/nvim/lua/core/plugin_config/copilot.lua
+++ .config/nvim/lua/core/plugin_config/copilot.lua
@@ -1,1 +1,3 @@
-vim.cmd[[highlight CopilotSuggestion ctermfg=8 guifg=white guibg=#5c6370]]
+vim.cmd [[highlight CopilotSuggestion ctermfg=8 guifg=white guibg=#5c6370]]
+
+vim.api.nvim_set_keymap('i', '<A-;>', 'copilot#Accept("<CR>")', { expr = true, silent = true })
