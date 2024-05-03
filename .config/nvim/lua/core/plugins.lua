1a0dec75be9596c90fc238f12b4dd66fb927e9db
Ahmed Yasser <ahmedYasser9@proton.me>
2024-05-03 10:22:47 +0300

    fixing markdown preview problem in neovim

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -31,7 +31,12 @@ require("lazy").setup({
   },
   {
     "iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
   },
   {
     "nvim-telescope/telescope.nvim",
