b13be31f3d81480c3c6948947535406eb2ba60a6
Ahmed Yasser <ahmedYasser9@proton.me>
2024-05-07 03:22:38 +0300

    updating lunarvim configs

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,141 +1,97 @@
--- Load custom keymaps and development settings
-require('keymaps')
--- require('development')
+-------------------------------------------------------------------------------- Options Start ----------------------------------------------------------------------------
 
-
--- Change theme settings
---lvim.colorscheme = "lunar"
---lvim.transparent_window = true
---lvim.builtin.lualine.options.theme = "lunar"
-
--- Change status line
-lvim.builtin.lualine.style = "default" -- or "none"
-
--- General Vim options
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.relativenumber = true
-
--- General LunarVim settings
-lvim.log.level = "info"
-lvim.format_on_save = {
-    enabled = true,
-    pattern = "*.lua",
-    timeout = 1000,
-}
-
--- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
-
--- Plugin settings
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-lvim.builtin.treesitter.auto_install = true
-
--- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
-lvim.plugins = {
-    -- {
-    --   "folke/trouble.nvim",
-    --   cmd = "TroubleToggle",
-    -- }
-    "github/copilot.vim"
-}
-
-
--- Automatically install missing parsers when entering buffer
-lvim.builtin.treesitter.auto_install = true
-
--- lvim.builtin.treesitter.ignore_install = { "haskell" }
-
--- -- always installed on startup, useful for parsers without a strict filetype
--- lvim.builtin.treesitter.ensure_installed = { "comment", "markdown_inline", "regex" }
-
--- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
-
--- --- disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
-
--- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "stylua" },
---   {
---     command = "prettier",
---     extra_args = { "--print-width", "100" },
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     command = "shellcheck",
---     args = { "--severity", "warning" },
---   },
--- }
-
-
-
+lvim.colorscheme = "everforest"
 lvim.format_on_save = true
+lvim.builtin.treesitter.auto_install = true
+lvim.transparent_window = true
+lvim.builtin.project.manual_mode = true
+
+-------------------------------------------------------------------------------- Options End ------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------- Keybindings Start ------------------------------------------------------------------------
 
 -- lvim.builtin.lualine.style = "default"
 lvim.keys.insert_mode["jk"] = "<Esc>"
 lvim.keys.insert_mode["kj"] = "<Esc>"
-lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
-lvim.keys.normal_mode["<S-h>"] = ":bprevious<CR>"
-lvim.keys.normal_mode["<A-a>"] = "ggvG"
-lvim.keys.normal_mode["<A-i>"] = ":set laststatus=0<CR>"
-lvim.keys.normal_mode["<A-o>"] = ":LspStop<CR>"
-lvim.keys.normal_mode["dj"] = '"jdd'
-lvim.keys.normal_mode["dk"] = '"kdd'
-lvim.keys.normal_mode["'j"] = '"jp'
-lvim.keys.normal_mode["'k"] = '"kp'
 
+-------------------------------------------------------------------------------- Keybindings End --------------------------------------------------------------------------
+
+
+-------------------------------------------------------------------------------- Telescope Start --------------------------------------------------------------------------
+
+local builtin = require('telescope.builtin')
+local action_state = require('telescope.actions.state')
+local actions = require('telescope.actions')
+
+lvim.builtin.telescope.defaults.layout_strategy = "horizontal";
+lvim.builtin.telescope.defaults.layout_config = {
+  width = 0.8,
+  height = 0.8,
+  prompt_position = "top",
+  preview_cutoff = 120,
+  horizontal = {
+    preview_width = 0.6,
+    results_width = 0.8,
+  },
+  vertical = {
+    width = 0.9,
+    height = 0.9,
+    preview_height = 0.5,
+  },
+}
+
+local buffer_searcher
+buffer_searcher = function()
+  builtin.buffers {
+    sort_mru = true,
+    show_all_buffers = false,
+    attach_mappings = function(prompt_bufnr, map)
+      local refresh_buffer_searcher = function()
+        actions.close(prompt_bufnr)
+        vim.schedule(buffer_searcher)
+      end
+      local delete_buf = function()
+        local selection = action_state.get_selected_entry()
+        vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+        refresh_buffer_searcher()
+      end
+      local delete_multiple_buf = function()
+        local picker = action_state.get_current_picker(prompt_bufnr)
+        local selection = picker:get_multi_selection()
+        for _, entry in ipairs(selection) do
+          vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+        end
+        refresh_buffer_searcher()
+      end
+      map('n', 'dd', delete_buf)
+      map('n', '<C-k>', delete_multiple_buf)
+      map('i', '<C-k>', delete_multiple_buf)
+      return true
+    end
+  }
+end
+
+lvim.keys.normal_mode['<C-i>'] = builtin.find_files
+lvim.keys.normal_mode['<C-o>'] = buffer_searcher
+lvim.keys.normal_mode['<C-p>'] = builtin.live_grep
+
+-------------------------------------------------------------------------------- Telescope End -----------------------------------------------------------------------------
+
+
+-------------------------------------------------------------------------------- Copilot Start -----------------------------------------------------------------------------
+
+vim.api.nvim_set_keymap('i', '<A-;>', 'copilot#Accept("<CR>")', { expr = true, silent = true })
+
+-------------------------------------------------------------------------------- Copilot End -------------------------------------------------------------------------------
+
+
+-------------------------------------------------------------------------------- Plugins Start -----------------------------------------------------------------------------
 
 lvim.plugins = {
-    {
-        "christoomey/vim-tmux-navigator"
-    },
-    {
-        "sainnhe/everforest"
-    },
-    {
-        "simrat39/rust-tools.nvim"
-    },
-    -- {
-    --   "iamcco/markdown-preview.nvim",
-    --   build = "cd app; npm install",
-    --   init = function() vim.g.mkdp_filetypes = { "markdown" } end,
-    --   ft = { "markdown" },
-    -- },
-    {
-        "kana/vim-textobj-user"
-    }, {
-    "lervag/vimtex"
+  "christoomey/vim-tmux-navigator",
+  "sainnhe/everforest",
+  "github/copilot.vim"
+
 }
-}
+
+-------------------------------------------------------------------------------- Plugins End -------------------------------------------------------------------------------
