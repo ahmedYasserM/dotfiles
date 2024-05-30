496b0ccb73977140c62ec2a66214bfdfda5b5ee4
Ahmed Yasser <ahmedYasser9@proton.me>
2024-05-30 08:53:03 +0300

    Update NVChad configs

--- .config/nvChad/lua/plugins/init.lua
+++ .config/nvChad/lua/plugins/init.lua
@@ -174,14 +174,6 @@ return {
     lazy = false,
   },
   {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      view = {
-        side = "right",
-      },
-    },
-  },
-  {
     "ahmedkhalf/project.nvim",
     config = function()
       require("project_nvim").setup {
@@ -189,6 +181,18 @@ return {
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
         silent_chdir = false,
+        -- lua
+        require("nvim-tree").setup {
+          view = {
+            side = "right",
+          },
+          sync_root_with_cwd = true,
+          respect_buf_cwd = true,
+          update_focused_file = {
+            enable = true,
+            update_root = true,
+          },
+        },
       }
     end,
   },
