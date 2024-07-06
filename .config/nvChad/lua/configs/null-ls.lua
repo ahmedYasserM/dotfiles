5430521ec31830f705b13082f6aa7a7d79ffbb01
Ahmed Yasser <ahmedYasser9@proton.me>
2024-07-06 10:22:40 +0300

    Add more textobjects to neovim

--- .config/nvChad/lua/configs/null-ls.lua
+++ .config/nvChad/lua/configs/null-ls.lua
@@ -1,21 +1,24 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
 local opts = {
   sources = {
     null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettier.with {
+      filetypes = { "html", "json", "yaml", "markdown", "astro" },
+    },
   },
   on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds {
         group = augroup,
         buffer = bufnr,
-      })
+      }
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
+          vim.lsp.buf.format { bufnr = bufnr }
         end,
       })
     end
